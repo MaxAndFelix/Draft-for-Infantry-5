0200d5e1529e2f77df1907066eb66da726ec54de
Doeca <i@doeca.cc>
2023-02-27 22:32:10 +0800

    add temperature control

--- Draft/Inc/app/PID.h
+++ Draft/Inc/app/PID.h
@@ -1,13 +1,39 @@
 ﻿#ifndef __PID_H
 #define __PID_H
 #include "main.h"
+#include "struct_typedef.h"
 void pid_init(pid_struct_t *pid,
               float kp,
               float ki,
               float kd,
               float i_max,
               float out_max);
-							
-float pid_calc(pid_struct_t *pid, float ref, float fdb);						
+
+float pid_calc(pid_struct_t *pid, float ref, float fdb);
+enum PID_MODE
+{
+    PID_POSITION = 0,
+    PID_DELTA
+};
+typedef struct
+{
+    uint8_t mode;
+    fp32 Kp;
+    fp32 Ki;
+    fp32 Kd;
+
+    fp32 max_out;
+    fp32 max_iout;
+
+    fp32 set;
+    fp32 fdb;
+
+    fp32 out;
+    fp32 Pout;
+    fp32 Iout;
+    fp32 Dout;
+    fp32 Dbuf[3];
+    fp32 error[3];
+
+} pid_type_def;
 #endif
-							
